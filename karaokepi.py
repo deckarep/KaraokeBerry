606c18beaf6d202f13531f6a5f22ea4061b407cd
Deckarep <deckarep@gmail.com>
2013-04-10 22:54:37 +0000

    added rudimentary login/logout code taken directly from Flask documentation of 0.9

--- karaokepi.py
+++ karaokepi.py
@@ -1,15 +1,38 @@
 
 
 #should redo virtualenv WITH site-packages so it sees the pykaraoke install
+#*this worked!!!!
 
+#python standard imports
 from subprocess import call
 
-from flask import Flask
+#flask specific imports
+from flask import Flask, session, redirect, url_for, escape, request
+
 app = Flask(__name__)
+app.debug = True
+app.secret_key = 'Booga Time!' #secret session key more info at Flask documentation site
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return "Hello World!, welcome to KaraokePi!"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form action="" method="post"><p><input type="text" name="username" /></p>
+        <p><input type="submit" value="login" /></p></form>'''
+
+@app.route('/logout')
+def logout():
+    #remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 @app.route("/play/<artist>")
 def play_artist(artist):
