b8af0d3d03e723171d4d49fcf486293c22b7b3ff
Deckarep <deckarep@gmail.com>
2013-04-12 01:39:50 +0000

    have rudimentary auto-suggest working and building out jquery mobile based web page for navigation this bad boy

--- karaokepi.py
+++ karaokepi.py
@@ -13,9 +13,17 @@ from flask import Flask, session, redirect, url_for, escape, request, jsonify
 
 SONG_PATH = '/home/pi/'
 
+pykaraoke_process = None
+
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'Booga Time!' #secret session key more info at Flask documentation site
+
+#static files test
+@app.route("/mobile")
+def mobile():
+    return redirect(url_for('static', filename='index.html'))
+
 
 @app.route("/")
 def index():
@@ -25,8 +33,20 @@ def index():
 
 @app.route("/songs")
 def songs():
-   files = glob.glob(os.path.join(SONG_PATH, '*.cdg'))
-   return jsonify(dict(count=len(files), songs=files))
+    files = glob.glob(os.path.join(SONG_PATH, '*.cdg'))
+    return jsonify(dict(count=len(files), songs=files))
+
+@app.route("/search/<keyword>")
+def search(keyword):
+    if keyword is not None:
+        keyword = keyword.lower()
+    resultlist = []
+    songlist = ['robert', 'ronaldo', 'adrian', 'barbara', 'rick', 'chris', 'sierra', 'daniel', 'chrisanne', 'ruben', 'jeremy', 'john', 'jimmy', 'jackson', 'jonas', 'mycal', 'melinda', 'melissa', 'steve', 'steven', 'steffan', 'jack', 'sonia', 'ralph', 'nicole', 'sophia', 'ronnie', 'jesse', 'adrianna', 'mia', 'aviara'] 
+    for token in songlist:
+        if keyword in token:  
+            resultlist.append(token)
+    return jsonify(dict(songs=resultlist))
+     
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -43,11 +63,22 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
+@app.route("/queue/<artist>")
+def queue_artist(artist):
+    return "TODO: queue_artist"
+
 @app.route("/play/<artist>")
 def play_artist(artist):
     #subprocess.call(['pykaraoke', os.path.join(SONG_PATH, 'pokerface.cdg')])
-    subprocess.Popen(['pykaraoke', os.path.join(SONG_PATH, 'pokerface.cdg')])
+    global pykaraoke_process
+    pykaraoke_process = subprocess.Popen(['pykaraoke', os.path.join(SONG_PATH, 'pokerface.cdg')])
     return "Now playing artist %s" % artist
+
+@app.route("/stop/")
+def stop_playing():
+    if pykaraoke_process is not  None:
+        pykaraoke_process.terminate()
+    return "stopping subprocess"
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
