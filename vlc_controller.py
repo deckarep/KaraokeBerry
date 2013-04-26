af7554c78db469da8b604d3833a7a1c14563dcdd
Ralph Caraveo <rcaraveo@loopnet.com>
2013-04-25 17:54:58 -0700

    not working with cli interface, can't get it to work right

--- vlc_controller.py
+++ vlc_controller.py
@@ -7,6 +7,7 @@
 #turn on Command Line (not RC (old))
 #when VLC is started from command line you can type help to get a list of supported commands
 import subprocess
+import time
 
 VLC_PATH = '/Applications/VLC.app/Contents/MacOS/VLC'
 
@@ -21,9 +22,16 @@ class Controller(object):
 	def is_alive(self):
 		return not(self.controller_process is None)
 
+	def command(self, commandText):
+		if self.is_alive():
+			print "Issue command: %s" % commandText		
+			stdoutdata, stderrdata = self.controller_process.communicate(input=commandText)
+			print 'stdoutdata: %s' + stdoutdata
+			print 'stderrdata: %s' + stderrdata
+
 	def toggle_fullscreen(self):
 		'''Toggles between fullscreen and regular'''
-		pass
+		self.command('fullscreen')
 
 	def enqueue_song(self, path_to_file):
 		'''Enqueue's a song to be played in the future'''
@@ -31,21 +39,23 @@ class Controller(object):
 
 	def stop(self):
 		'''Stops current stream from playing'''
-		if self.is_alive():
-			(stdoutdata, stderrdata) = self.controller_process.communicate('stop\n')
+		self.command('stop')
 		
 
 	def toggle_pause(self):
 		'''Toggles the state of the stream from paused to not paused'''
-		pass
+		self.command('pause')
 
-	def play(self, path_to_file):
+	def play(self):
+		self.command('play')
+
+	def start(self, path_to_file):
 		'''Plays a song immediatley whether or not one is already playing'''
 		#opens in another process
-		if self.controller_process is None:
-			self.controller_process = subprocess.Popen([VLC_PATH, '--fullscreen', '--play-and-exit', '--video-on-top', '--video-on-top', path_to_file], stdin=subprocess.PIPE)
-
+		#self.controller_process = subprocess.Popen([VLC_PATH, ' --play-and-exit ', ' --fullscreen ', ' --video-on-top ', path_to_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		self.controller_process = subprocess.Popen([VLC_PATH, path_to_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)		
 		self.history.append(path_to_file)        	
+		print 'Process started with file: %s' % path_to_file
 
 	def is_playing(self):
 		'''Indicates whether or not vlc is in the middle of a stream'''
@@ -57,6 +67,16 @@ class Controller(object):
 
 	def quit(self):
 		'''Terminates the player instance'''
-		pass
+		self.controller_process.terminate()
 
+def main():
+	c = Controller()
+	c.start('/Users/ralphcaraveo/Karaoke/LeAnn Rimes - How Do I Live.mp3')
+	
+	time.sleep(10)
+	c.toggle_fullscreen()
+	
+
+if __name__ == '__main__':
+	main()
 
